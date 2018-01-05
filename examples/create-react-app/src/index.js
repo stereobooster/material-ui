7ccbfa3c1acf9b53f4712f229e53e6d8688218ca
stereobooster <stereobooster@gmail.com>
2018-01-05 22:21:38 +0100

    Another fix

--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -4,35 +4,20 @@ import { loadComponents } from 'loadable-components';
 import { getState } from 'loadable-components/snap';
 import Index from './pages/index';
 
-import JssProvider from 'react-jss/lib/JssProvider';
-import { create } from 'jss';
-import preset from 'jss-preset-default';
-const createGenerateClassName = () => {
-  let counter = 0;
-  return (rule, sheet) =>
-    `c${Math.random()
-      .toString(36)
-      .substring(2, 4) +
-      Math.random()
-        .toString(36)
-        .substring(2, 4)}-${rule.key}-${counter++}`;
-};
-const jss = create(preset());
-// Custom Material-UI class name generator for better debug and performance.
-jss.options.createGenerateClassName = createGenerateClassName;
-const app = (
-  <JssProvider jss={jss}>
-    <Index />
-  </JssProvider>
-);
-
+const app = <Index />;
 const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  loadComponents().then(() => {
-    hydrate(app, rootElement);
-  });
-} else {
-  render(app, rootElement);
-}
 
-window.snapSaveState = () => getState();
+loadComponents().then(() => {
+  render(app, rootElement, () => {
+    Array.from(document.querySelectorAll('[data-jss-snap]')).forEach(elem =>
+      elem.parentNode.removeChild(elem),
+    );
+  });
+});
+
+window.snapSaveState = () => {
+  Array.from(document.querySelectorAll('[data-jss]')).forEach(elem =>
+    elem.setAttribute('data-jss-snap', ''),
+  );
+  return getState();
+};
