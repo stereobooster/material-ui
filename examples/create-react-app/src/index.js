f48de5dea18b7f777c28d615621f592c61eee15c
stereobooster <stereobooster@gmail.com>
2018-01-05 21:50:25 +0100

    Fix bug

--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
-import { loadComponents } from "loadable-components";
-import { getState } from "loadable-components/snap";
+import { loadComponents } from 'loadable-components';
+import { getState } from 'loadable-components/snap';
 import Index from './pages/index';
 
-window.snapSaveState = () => getState();
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import preset from 'jss-preset-default';
+const createGenerateClassName = () => {
+  let counter = 0;
+  return (rule, sheet) =>
+    `c${Math.random()
+      .toString(36)
+      .substring(2, 4) +
+      Math.random()
+        .toString(36)
+        .substring(2, 4)}-${rule.key}-${counter++}`;
+};
+const jss = create(preset());
+// Custom Material-UI class name generator for better debug and performance.
+jss.options.createGenerateClassName = createGenerateClassName;
+const app = (
+  <JssProvider jss={jss}>
+    <Index />
+  </JssProvider>
+);
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
   loadComponents().then(() => {
-    hydrate(<Index />, rootElement);
+    hydrate(app, rootElement);
   });
 } else {
-  render(<Index />, rootElement);
+  render(app, rootElement);
 }
+
+window.snapSaveState = () => getState();
